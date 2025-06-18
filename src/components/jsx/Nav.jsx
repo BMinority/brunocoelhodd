58916bbdac948b18a26a65f2d6267b5cbde376aa
bruno coelho <brunocoelho66@gmail.com>
2025-06-18 20:47:24 -0300

    style: add active class to nav component

--- src/components/jsx/Nav.jsx
+++ src/components/jsx/Nav.jsx
@@ -6,9 +6,11 @@ import Social from './Social';
 
 function Nav({ onNavigate }) {
     const [menuOpen, setMenuOpen] = useState(false);
+    const [activeSection, setActiveSection] = useState('home');
 
     const handleNavigation = (section) => {
         onNavigate(section);
+        setActiveSection(section);
         setMenuOpen(false);
     };
 
@@ -34,10 +36,22 @@ function Nav({ onNavigate }) {
                     </div>
 
                     <ul className='bc-nav-links'>
-                        <li onClick={() => handleNavigation('home')}>Início</li>
-                        <li onClick={() => handleNavigation('galery')}>Galeria</li>
-                        <li onClick={() => handleNavigation('projects')}>Projetos</li>
-                        <li onClick={() => handleNavigation('contact')}>Contato</li>
+                        <li className={activeSection === 'home' ? 'active' : ''}
+                            onClick={() => handleNavigation('home')}>
+                            <i class="fa-solid fa-house-chimney-user"></i> Início
+                        </li>
+                        <li className={activeSection === 'galery' ? 'active' : ''}
+                            onClick={() => handleNavigation('galery')}>
+                            <i class="fa-solid fa-image"></i> Galeria
+                        </li>
+                        <li className={activeSection === 'projects' ? 'active' : ''}
+                            onClick={() => handleNavigation('projects')}>
+                            <i class="fa-solid fa-globe"></i> Projetos
+                        </li>
+                        <li className={activeSection === 'contact' ? 'active' : ''}
+                            onClick={() => handleNavigation('contact')}>
+                            <i class="fa-solid fa-paper-plane"></i> Contato
+                        </li>
                     </ul>
 
                     <Social />
